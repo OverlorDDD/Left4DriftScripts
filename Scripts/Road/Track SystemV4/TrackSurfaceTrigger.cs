ed4dec266f3d613aa1c15c2eb187ef94bfb6b084
OverlorDDD <denavramenko777@gmail.com>
2026-07-22 13:13:09 +0300

    Delete Scripts directory

--- Scripts/Road/Track SystemV4/TrackSurfaceTrigger.cs
+++
@@ -1,9 +0,0 @@
-using UnityEngine;
-
-public class TrackSurfaceTrigger : MonoBehaviour
-{
-    public SurfaceType surfaceType = SurfaceType.Asphalt;
-
-    // CarController читає цей компонент через OnCollisionStay
-    // і застосовує множники з TrackSurfaceProperties
-}
